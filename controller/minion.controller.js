6204654cd99611bdc0369d1d17aa4e9b3773dbd7
Mosiur <mosiurislamwebdesign@gmail.com>
2023-01-20 12:31:21 +0600

    complied wrok

--- controller/minion.controller.js
+++ controller/minion.controller.js
@@ -1,17 +1,72 @@
 const { MinionModel } = require("../model/minion.Schema")
-
+/*  Minion ad */
 const addMinion = async (req, res) => {
     try {
         const data = req.body
+        let result;
+        if (data) {
+            result = await MinionModel.create(data)
+        }
+        if (result) {
+            return res.status(201).json({ result })
+        }
 
-        const result = await MinionModel.create(data)
-
-        return res.status(201).json({ contact })
     } catch (err) {
         const errorMessage = errorMessageFormatter(err)
         return res.status(500).json(errorMessage)
     }
 }
+/*  Minion Get */
+const GetMinion = async (req, res) => {
+    try {
+        const result = await MinionModel.find({})
+        return res.status(201).json({ result })
+
+    } catch (err) {
+        const errorMessage = errorMessageFormatter(err)
+        return res.status(500).json(errorMessage)
+    }
+}
+/*  Minion Update */
+
+const updateMinion = async (req, res) => {
+    try {
+        const id = req.params.id
+        const data = req.body
+        let result;
+        if (data && id) {
+            result = await MinionModel.updateMany({ _id: id }, { $set: data }, {
+                runValidators: true,
+            })
+        }
+        if (result) {
+            return res.status(200).json(result)
+        }
+
+    } catch (err) {
+        const errorMessage = errorMessageFormatter(err)
+        return res.status(500).json(errorMessage)
+    }
+}
+/*  Minion delete */
+const DeleteMinion = async (req, res) => {
+    try {
+        const id = req.params.id
+        let result;
+        if (id) {
+            result = await MinionModel.findByIdAndDelete({ _id: id })
+        }
+        if (result) {
+            return res.status(200).json(result)
+        }
+
+    } catch (err) {
+        const errorMessage = errorMessageFormatter(err)
+        return res.status(500).json(errorMessage)
+    }
+}
+
+
 module.exports = {
-    addMinion,
+    addMinion, GetMinion, updateMinion, DeleteMinion
 }
--- routes/minions.js
+++ routes/minions.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
+const { addMinion, GetMinion, updateMinion, DeleteMinion } = require('../controller/minion.controller')
 const minionsRoutes = Router()
 
 
 // contact routes
-minionsRoutes.post('/minion',)
-minionsRoutes.get('/minions',)
-minionsRoutes.put('/minion',)
-minionsRoutes.delete('/minion',)
+minionsRoutes.post('/minion', addMinion)
+minionsRoutes.get('/minions', GetMinion)
+minionsRoutes.put('/minion/:id', updateMinion)
+minionsRoutes.delete('/minion/:id',DeleteMinion)
 
 module.exports = {
     minionsRoutes
